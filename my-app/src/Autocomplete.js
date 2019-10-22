91418dac987b62fefda54be246ea9fdaf6cf3a52
rezvan-hb <rezvanhabibollahi1995@gmail.com>
2019-10-22 23:44:38 +0330

    'final'

--- my-app/src/Autocomplete.js
+++ my-app/src/Autocomplete.js
@@ -8,70 +8,107 @@ class Autocomplete extends Component {
     super(props);
     this.state = {
         filtered:[],
+        isOpen : false,
+        list: this.props.items,
     };
     }
 
-    componentWillReceiveProps(nextProps) {
+    // componentDidUpdate(nextstate){
+    //     this.setState({
+    //         filtered: nextstate.items
+    //       },[]);
+    // }
+
+    toggle = () => this.setState(!this.state.isOpen);
+    handleClickOutside = () => {
+        // var hideMe = document.getElementById('hideMe');
+        // if (hideMe){
+        //     hideMe.style.display = 'none';
+        // }
         this.setState({
-          filtered: nextProps.items
+            filtered:[],
+            isOpen: false
         });
     }
-        
-    handleClickOutside = () => {
-        var hideMe = document.getElementById('hideMe');
-        if (hideMe){
-            hideMe.style.display = 'none';
-        }
-    }
-    
+
     handleChange(e) {
+        console.log('e.target.value:', e.target.value)
         let totalCountry = [];
         let searchCountry = [];
-        console.log('items', this.props.items) 
+        console.log('items', this.state.list) 
         if (e.target.value !== "") {
-            totalCountry = this.props.items;  
+            totalCountry = this.state.list;  
             searchCountry = totalCountry.filter(item => {
             const lc = item.toLowerCase();
             const filter = e.target.value.toLowerCase();
             return lc.startsWith(filter);
         })
         } else {
-          searchCountry = [];
+          searchCountry = this.state.list;
         }
         console.log('searchCountry', searchCountry)
         this.setState({
             filtered: searchCountry
         });
     }
+
+    // add focus event
+    handleFocus(e){
+        this.handleChange(e)
+    }
+    
     getValue(e){
         var dataId = e.currentTarget.dataset.id 
-        var countryName = this.state.filtered[dataId]
+        var itemName = this.state.filtered[dataId]
         this.setState({filtered:[]});
-        document.getElementById("search").value = countryName;
+        document.getElementById("search").value = itemName;
+        var item = {}
+        item.district = 'tehran'
+        item.religion = 'Islam'
+        item.country = itemName
+        this.props.onSelectItem(item);            
     }
 
     removeItem(item) {
-        var filtered = this.state.filtered;
-        console.log('filtered.length', filtered.length)
-        if (filtered.length !== 0){
-            filtered.some((el,i) => {
+        var newList = this.state.list;
+        var newFilter = this.state.filtered;
+
+        if (newList.length !== 0){
+            newList.some((el,i) => {
                 if (el === item) {
-                    filtered.splice(i,1);
-                    return true
+                    newList.splice(i,1);
+                return true
                 }
             });
-            this.setState({
-                filtered
+            this.setState({list:newList})
+        }
+
+        if (newFilter.length !== 0){
+            newFilter.some((el,i) => {
+                if (el === item) {
+                    newFilter.splice(i,1);
+                return true
+                }
             });
-        } 
+            this.setState({filtered:newFilter})
+        }
+
     }
     
     render() {
+        console.log('isOpen:::',this.state.isOpen)
         return (
-            <div className="container" onClick={ () => this.handleClickOutside}>
-                <input className='searchInput' placeholder="Search country..." id="search" type="search" onChange={(event) => this.handleChange(event)} /><br />
-                {this.state.filtered.length !==0 && 
-                    <div className="countrySearch" id='hideMe'>
+            <div className="container" onClick= { () => this.handleClickOutside} >
+                <input className='searchInput'
+                    placeholder="Search country..." 
+                    id="search" 
+                    type="search" 
+                    onChange={(event) => this.handleChange(event)} 
+                    onFocus = {(e) => this.handleFocus(e)}
+                    placeholder="Search..." 
+                /><br />
+                {this.state.filtered.length !==0 &&
+                    <div className="countrySearch" >
                         <ul>
                             {this.state.filtered.map((item ,index) => (
                                 <li key={item} > 
@@ -84,10 +121,11 @@ class Autocomplete extends Component {
                                 </li>
                             ))}
                         </ul>
-                    </div>
-                }   
-            </div>
+                    </div>   
+                }              
+            </div>       
         )
     }
 }
+
 export default onClickOutside(Autocomplete);
